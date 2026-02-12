56d844a86bce7b0a3a4ccd653f2beb59ebb67195
VardanHeroic <tarayanvardan@gmail.com>
2026-02-12 09:51:26 +0400

    error handling for 404 and others

--- src/telegram-scraper.js
+++ src/telegram-scraper.js
@@ -1,307 +1,265 @@
 //! Copyright Nuzulul Zulkarnain. Released under The MIT License.
 //! telegram-scraper -- https://github.com/nuzulul/telegram-scraper
 
-import html_parser from './html-parser.js'
+import html_parser from "./html-parser.js"
 
-import https from 'https'
+import https from "https"
 
 export async function telegram_scraper(channel) {
-
 	function HtmlEntitiesDecode(string) {
-		string = string.replace(/&quot;/g, '"');
-		string = string.replace(/&amp;/g, '&');
-		string = string.replaceAll('&nbsp;',' ')
-		string = string.replaceAll("&#39;","'")
-		return string;
+		string = string.replace(/&quot;/g, '"')
+		string = string.replace(/&amp;/g, "&")
+		string = string.replaceAll("&nbsp;", " ")
+		string = string.replaceAll("&#39;", "'")
+		return string
 	}
 
-	function get(targeturl,resolve){
-	
-			https.get(targeturl, res => {
-			
-				if((res.statusCode === 301 || res.statusCode === 302)&&(res.headers.location != undefined)){
-				
-					return get(res.headers.location,resolve)
-				
+	function get(targeturl, resolve, reject) {
+		https.get(targeturl, res => {
+			if ((res.statusCode === 301 || res.statusCode === 302) && res.headers.location != undefined) {
+				return get(res.headers.location, resolve)
+			}
+
+			if (res.statusCode / 100 > 4) {
+				reject(new Error(res.statusCode))
+			}
+
+			let data = []
+
+			res.on("data", chunk => {
+				data.push(chunk)
+			})
+
+			res.on("end", () => {
+				try {
+					const raw = Buffer.concat(data).toString()
+
+					resolve(raw)
+				} catch (err) {
+					let msg = JSON.stringify({ status: "error", msg: err, targeturl })
+
+					resolve(msg)
 				}
-			
-				let data = []
-				
-				res.on('data', chunk => { data.push(chunk) }) 
-
-				res.on('end', () => {
-				
-					try{
-				
-						const raw = Buffer.concat(data).toString()
-				   
-						resolve(raw)
-						
-					} catch(err) {
-					
-						let msg = JSON.stringify({status:'error',msg:err,targeturl})
-						
-						resolve(msg)
-					
-					}
-
-				})
-			}) 			
-	}
-
-	async function get_page(targeturl){
-
-		return new Promise((resolve) => {
-		
-			get(targeturl,resolve)
-			
+			})
 		})
 	}
-	
-	const targeturl = 'https://t.me/s/'+channel
-	
-	const rawdata = await get_page(targeturl)
-	
+
+	async function get_page(targeturl) {
+		return new Promise((resolve, reject) => {
+			get(targeturl, resolve, reject)
+		})
+	}
+
+	const targeturl = "https://t.me/s/" + channel
+	let rawdata = null
+	try {
+		rawdata = await get_page(targeturl)
+	} catch (error) {
+		console.log(error)
+		throw error
+	}
+
 	let temp = rawdata
-	
-	if (temp.indexOf('<section class="tgme_channel_history js-message_history">') == -1) return 'Unknown telegram channel'
-	
+
+	if (temp.indexOf('<section class="tgme_channel_history js-message_history">') == -1) return "Unknown telegram channel"
+
 	temp = temp.split('<section class="tgme_channel_history js-message_history">')
-	
-	temp = temp[1].split('</section>')
-	
+
+	temp = temp[1].split("</section>")
+
 	temp = temp[0].split('<div class="tgme_widget_message_wrap js-widget_message_wrap">')
-	
+
 	temp.splice(0, 1)
-	
+
 	let data = []
-	
-	for(var i=0;i<temp.length;i++){
-		
-		let html = '<div class="tgme_widget_message_wrap js-widget_message_wrap">'+temp[i]
-		
+
+	for (var i = 0; i < temp.length; i++) {
+		let html = '<div class="tgme_widget_message_wrap js-widget_message_wrap">' + temp[i]
+
 		let arr = html_parser(html)
-		
-		let message_text = ''
-		
+
+		let message_text = ""
+
 		let message_photo = []
-		
+
 		let message_video = []
-		
-		let views = ''
-		
-		let datetime = ''
-		
+
+		let views = ""
+
+		let datetime = ""
+
 		let include = true
-		
-		for(var j=0;j<arr[0].children[0].children[3].children.length;j++){
-			
-			let child_class = ''
-			
-			try{child_class = arr[0].children[0].children[3].children[j].attributes.class}catch{}
-			
-			if (child_class == "tgme_widget_message_text js-message_text"){
-				
-				function message_extractor(input){
-					
-					let msg = ''
-					
-					function loop(arr){
-						
-						for(let k=0;k<arr.length;k++){
-						
-							if (arr[k].text != null ) msg += arr[k].text
-							
-							try{if (arr[k].attributes.class == "tgme_widget_service_strong_text") include = false }catch{}
-							
-							if (arr[k].children != null){
-							
+
+		const post = arr[0].children[0].children[3].children //tgme widget mesage bubble its one
+
+		for (var j = 0; j < post.length; j++) {
+			let child_class = ""
+			try {
+				child_class = post[j].attributes.class
+			} catch { }
+
+			if (child_class == "tgme_widget_message_text js-message_text") {
+				function message_extractor(input) {
+					let msg = ""
+
+					function loop(arr) {
+						for (let k = 0; k < arr.length; k++) {
+							if (arr[k].text != null) msg += arr[k].text
+
+							try {
+								if (arr[k].attributes.class == "tgme_widget_service_strong_text") include = false
+							} catch { }
+
+							if (arr[k].children != null) {
 								if (arr[k].children.length > 0) loop(arr[k].children)
-							
 							}
-						
 						}
-						
 					}
-					
+
 					loop(input)
-					
-					return msg.replaceAll('/>',' ')
-				
+
+					return msg.replaceAll("/>", " ")
 				}
-				
-				let message_node = arr[0].children[0].children[3].children[j].children
-				
+
+				let message_node = post[j].children
+
 				message_text = message_extractor(message_node)
-				
 			}
-			
+
 			let arr_child_class = child_class.split(" ")
-			
-			if(arr_child_class[0] == "tgme_widget_message_photo_wrap"){
-				
-				message_photo.push(arr[0].children[0].children[3].children[j].attributes.style.split("'")[1])
+
+			if (arr_child_class[0] == "tgme_widget_message_photo_wrap") {
+				message_photo.push(post[j].attributes.style.split("'")[1])
 			}
-			
-			if(child_class == "tgme_widget_message_footer compact js-message_footer"){
-				
-				let get_views = arr[0].children[0].children[3].children[j].children[1].children[1].children[0].text
-				
+
+			if (child_class == "tgme_widget_message_footer compact js-message_footer") {
+				let get_views = post[j].children[1].children[1].children[0].text
+
 				if (get_views != null) views = get_views
-				
-				try{datetime = arr[0].children[0].children[3].children[j].children[1].children[3].children[0].children[0].attributes.datetime}catch{}
-				
-				try{datetime = arr[0].children[0].children[3].children[j].children[1].children[3].children[2].children[0].attributes.datetime}catch{}
-				
-			}
-			
-			if(child_class == "tgme_widget_message_video_player blured js-message_video_player"){
-				
-				message_video.push(arr[0].children[0].children[3].children[j].children[4].children[1].attributes.src)
-				
-			}
-			
-			if(child_class == "tgme_widget_message_video_player js-message_video_player"){
-				
-				message_video.push(arr[0].children[0].children[3].children[j].children[2].children[1].attributes.src)
-				
-			}
-			
-			if(child_class == "media_supported_cont"){
-				
-				try{
-					
-					let src = arr[0].children[0].children[3].children[j].children[0].children[2].attributes.src
-					
-					if(src != null)message_video.push(src)
-					
-				}catch{}
-				
-				try{
-					
-					let src = arr[0].children[0].children[3].children[j].children[0].children[2].children[1].attributes.src
-					
-					if(src != null)message_video.push(src)
-					
-				}catch{}
-				
-				try{
 
-					function message_extractor(input){
-						
-						let msg = ''
-						
-						function loop(arr){
-							
-							for(let k=0;k<arr.length;k++){
-							
-								if (arr[k].text != null ) msg += arr[k].text
-								
-								try{if (arr[k].attributes.class == "tgme_widget_service_strong_text") include = false }catch{}
-								
-								if (arr[k].children != null){
-								
+				try {
+					datetime = post[j].children[1].children[3].querySelector(".tgme_widget_message_meta").children[0].attributes.datetime
+				} catch { }
+
+				// try {
+				// 	datetime = post[j].children[1].children[3].children[1].children[0].attributes.datetime
+				// } catch {}
+			}
+
+			if (child_class == "tgme_widget_message_video_player blured js-message_video_player") {
+				message_video.push(post[j].children[4].children[1].attributes.src)
+			}
+
+			if (child_class == "tgme_widget_message_video_player js-message_video_player") {
+				message_video.push(post[j].children[2].children[1].attributes.src)
+			}
+
+			if (child_class == "media_supported_cont") {
+				try {
+					let src = post[j].children[0].children[2].attributes.src
+
+					if (src != null) message_video.push(src)
+				} catch { }
+
+				try {
+					let src = post[j].children[0].children[2].children[1].attributes.src
+
+					if (src != null) message_video.push(src)
+				} catch { }
+
+				try {
+					function message_extractor(input) {
+						let msg = ""
+
+						function loop(arr) {
+							for (let k = 0; k < arr.length; k++) {
+								if (arr[k].text != null) msg += arr[k].text
+
+								try {
+									if (arr[k].attributes.class == "tgme_widget_service_strong_text") include = false
+								} catch { }
+
+								if (arr[k].children != null) {
 									if (arr[k].children.length > 0) loop(arr[k].children)
-								
 								}
-							
 							}
-							
 						}
-						
+
 						loop(input)
-						
-						return msg.replaceAll('/>',' ')
-					
+
+						return msg.replaceAll("/>", " ")
 					}
-					
-					let message_node = arr[0].children[0].children[3].children[j].children[1].children
-					
+
+					let message_node = post[j].children[1].children
+
 					message_text = message_extractor(message_node)
-					
-				}catch{}
-				
+				} catch { }
 			}
-			
-			if (child_class == 	"tgme_widget_message_grouped_wrap js-message_grouped_wrap"){
-				
-				function loop(node){
-					
-					for(let k=0;k<node.length;k++){
-						
-						try{
-						
-							if (node[k].attributes.class != null){
 
-								if (node[k].attributes.class == "tgme_widget_message_video js-message_video") message_video.push(node[k].attributes.src)
-							
+			if (child_class == "tgme_widget_message_grouped_wrap js-message_grouped_wrap") {
+				function loop(node) {
+					for (let k = 0; k < node.length; k++) {
+						try {
+							if (node[k].attributes.class != null) {
+								if (node[k].attributes.class == "tgme_widget_message_video js-message_video")
+									message_video.push(node[k].attributes.src)
 							}
-							
-						}catch{}
+						} catch { }
 
-						try{
-						
-							if (node[k].attributes.class != null){
-
-								if (node[k].attributes.class == "tgme_widget_message_photo_wrap grouped_media_wrap blured js-message_photo") {
-									
+						try {
+							if (node[k].attributes.class != null) {
+								if (
+									node[k].attributes.class == "tgme_widget_message_photo_wrap grouped_media_wrap blured js-message_photo"
+								) {
 									let photo = node[k].attributes.style.split("'")[1]
-									
+
 									message_photo.push(photo)
-									
 								}
-							
 							}
-							
-						}catch{}
-						
-						if (node[k].children != null){
-							
+						} catch { }
+
+						if (node[k].children != null) {
 							if (node[k].children.length > 0) loop(node[k].children)
-							
 						}
-						
 					}
 				}
-				
-				let node = arr[0].children[0].children[3].children[j].children
-				
+
+				let node = post[j].children
+
 				loop(node)
 			}
 		}
-		
+
 		let item = {
-			
 			//raw:arr[0],
-			
+
 			data_post: arr[0].children[0].attributes["data-post"],
-			
+
 			data_view: arr[0].children[0].attributes["data-view"],
-			
+
 			user_url: arr[0].children[0].children[1].children[0].attributes.href,
-			
+
 			user_photo: arr[0].children[0].children[1].children[0].children[0].children[0].attributes.src,
-			
-			user_name: arr[0].children[0].children[3].children[3].children[0].children[0].children[0].text,
-			
-			message_url:'https://t.me/'+arr[0].children[0].attributes["data-post"],
-			
-			message_text:HtmlEntitiesDecode(message_text),
-			
+
+			user_name: post[3].children[0].children[0].children[0].text,
+
+			message_url: "https://t.me/" + arr[0].children[0].attributes["data-post"],
+
+			message_text: HtmlEntitiesDecode(message_text),
+
 			message_photo,
-			
+
 			message_video,
-			
+
 			views,
-			
-			datetime
+
+			datetime,
 		}
-		
-		if(include)data.push(item)
+
+		if (include) data.push(item)
 	}
-	
+
 	let result = JSON.stringify(data, null, 30)
-	
+
 	return result
-	
 }
--- test/server.js
+++ test/server.js
@@ -1,38 +1,52 @@
-const {telegram_scraper} = require('./../src/telegram-scraper')
+const { telegram_scraper } = require("./../src/telegram-scraper")
+const http = require("http")
 
-const http = require('http')
+const serverport = process.env.PORT || 8080
 
-const serverport =  process.env.PORT || 8080
+function Item(id, url, content_text, images, date_published, videos) {
+	this.id = id
+	this.url = url
+	this.title = content_text.slice(0, 100) + "..."
+	this.content_html = `
+		${videos.reduce((fullString, url) => fullString + `<video src=${url} alt=${url}/>`, "")}
+		${images.reduce((fullString, url) => fullString + `<img src=${url} alt=${url}/>`, "")}
+		<p>${content_text}</p>`
+	this.date_published = date_published
+}
 
-let telegram_channel = 'telegram'
-
-const server = http.createServer(async(req,res)=>{
-
-	let result = await telegram_scraper(telegram_channel)
+const server = http.createServer(async (req, res) => {
+	const telegram_channel = req.url.slice(1)
 
 	res.statusCode = 200
-	
-	res.setHeader('Content-Type','application/json')
-	
-	res.setHeader('Access-Control-Allow-Origin','*')
-	
-	res.end(result)
-	
+	res.setHeader("Content-Type", "application/feed+json")
+	res.setHeader("Access-Control-Allow-Origin", "*")
+
+	// if (telegram_channel === "feed" || telegram_channel === "feed.xml" || telegram_channel === "rss" || telegram_channel === "rss.xml") {
+	// 	res.end(null)
+	// }
+
+	console.log(telegram_channel)
+
+	try {
+		const data = await telegram_scraper(telegram_channel)
+		const result = JSON.parse(data)
+		const feed = {
+			version: "https://jsonfeed.org/version/1",
+			title: result[0]["user_name"],
+			description: "Feed generated from telegram",
+			icon: result[0]["user_photo"],
+			items: result.map(
+				post =>
+					new Item(post.data_post, post.message_url, post.message_text, post.message_photo, post.datetime, post.message_video),
+			),
+		}
+		res.end(JSON.stringify(feed, null, 4))
+	} catch (error) {
+		res.setHeader("StatusCode", error.message)
+		res.end(error.message)
+	}
 })
 
-server.listen(serverport,()=>{
-
-	console.log(`Server running at ${serverport}`) 
-	
+server.listen(serverport, () => {
+	console.log(`Server running at ${serverport}`)
 })
-
-
-/*
-void async function main() {
-	let telegram_channel_username = 'telegram'
-
-	let result = await telegram_scraper(telegram_channel_username)
-
-	console.log(result)
-}()
-*/
